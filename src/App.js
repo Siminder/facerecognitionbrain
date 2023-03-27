3be2a4d2c3837ab4ff7b7d5955eb82940621a0f8
Siminder <Bansalsiminder234@gmail.com>
2023-03-27 01:16:42 -0400

    Frontend part for smartbrain

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
+import Logo from './components/Logo';
+import ImageLinkForm from './components/ImageLinkForm.js';
+import Rank from './components/Rank.js';
+import { Component } from 'react';
+import ParticlesBg from 'particles-bg'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ParticlesBg type="circle" bg={true} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition />*/}
     </div>
   );
 }
