d5607a55f03daaf2487a880de40881c3491551e4
Siminder <Bansalsiminder234@gmail.com>
2023-03-29 02:17:21 -0400

    adding facical recognition

--- src/App.js
+++ src/App.js
@@ -2,23 +2,102 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Logo from './components/Logo';
 import ImageLinkForm from './components/ImageLinkForm.js';
+import FaceRecognition from './components/FaceRecognition.js';
 import Rank from './components/Rank.js';
 import { Component } from 'react';
 import ParticlesBg from 'particles-bg'
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifyFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage')
+    const width  = Number(image.width )
+    const height = Number(image.height)
+    console.log(clarifyFace)
+    console.log(width, height)
+
+    return{
+      leftCol: clarifyFace.left_col * width,
+      topRow: clarifyFace.top_row * height,
+      rightCol: width - (clarifyFace.right_col * width),
+      bottomRow: height - (clarifyFace.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+
+    const PAT = '99fdf6f847c34361980880107d7010e1';
+    const USER_ID = 'simi';       
+    const APP_ID = 'SmartBrain';
+ 
+    const MODEL_ID = 'face-detection'
+    const IMAGE_URL = this.state.input;
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+
+   const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+};
 
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg type="circle" bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
-    </div>
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", requestOptions)
+        .then(response => response.json())
+        .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+        .catch(err => console.log(err));
+}
+
+  render() {  
+    return (
+      <div className="App">
+        <ParticlesBg type="circle" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box}  />
+      </div>
   );
+ }
 }
 
 export default App;
